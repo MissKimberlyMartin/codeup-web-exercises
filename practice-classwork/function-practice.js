bb8b0aa8ab867305a859dee6e0f9aa59880bc103
Kimberly Martin <kim.a.martin33@gmail.com>
2020-10-19 07:37:53 -0500

    updated selectors exercise

--- practice-classwork/function-practice.js
+++
@@ -1,6 +0,0 @@
-(function ()    {
-    "use strict";
-    // add JS code here
-})();
-
-
--- practice-misc-files/external-js.js
+++
@@ -1,9 +0,0 @@
-// External JS file practice 24Sep
-
-"use strict";
-
-console.log("This is my external JS file");
-console.log("This is the second console message from the external JS file");
-
-var myAge = 50;
-console.log(myAge);
